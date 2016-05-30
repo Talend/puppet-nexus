7efba055c38f81778284714de0034e87eb35353d
Alexander Grigoriev <agrigoriev@talend.com>
2016-05-30 08:18:11 +0200

    class spec updated

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,43 +1,40 @@
 require 'spec_helper_acceptance'
 
-describe 'apt class' do
+describe 'nexus' do
 
-  context 'default parameters' do
-    # Using puppet_apply as a helper
-    it 'should work with no errors' do
-      pp = <<-EOS
-      class{ '::java': }
+  context 'with default parameters' do
+    it_should_behave_like 'nexus::installed', "
+      version  => '2.8.0',
+      revision => '05'
+    "
+    it_should_behave_like 'nexus::running'
 
-      class{ '::nexus':
-        version    => '2.8.0',
-        revision   => '05',
-        nexus_root => '/srv',
-      }
-      EOS
-
-      # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_failures => true)
+    describe file('/srv/sonatype-work/nexus/conf/security-configuration.xml') do
+      it { should be_file }
+      its(:content) { should include '<anonymousAccessEnabled>false</anonymousAccessEnabled>' }
+      its(:content) { should include '<realm>XmlAuthenticatingRealm</realm>' }
+      its(:content) { should include '<realm>XmlAuthorizingRealm</realm>' }
     end
 
-    describe user('nexus') do
-      it { should belong_to_group 'nexus' }
-    end
-
-    describe service('nexus') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
-
-    context 'Nexus should be running on the default port' do
-      describe command('sleep 30 && echo "Give Nexus time to start"') do
-        its(:exit_status) { should eq 0 }
-      end
-
-      describe command('curl 0.0.0.0:8081/nexus/') do
-        its(:stdout) { should match /Sonatype Nexus&trade; 2.8.0-05/ }
+    describe 'nexus user deployment should be removed' do
+      describe file('/srv/sonatype-work/nexus/conf/security.xml') do
+        it { should be_file }
+        its(:content) { should_not include '<id>deployment</id>' }
       end
     end
-
   end
+
+  context 'with download_folder parameter set to /var/tmp' do
+    it_should_behave_like 'nexus::installed', "
+      version          => '2.8.0',
+      revision        =>  '05',
+      download_folder =>  '/var/tmp/'
+    "
+    it_should_behave_like 'nexus::running'
+
+    describe file('/var/tmp/nexus-2.8.0-05-bundle.tar.gz') do
+      it { should be_file }
+    end
+  end
+
 end
