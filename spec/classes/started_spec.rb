ef559716589c926a08763fc7c24e0d8594e79e4e
Alexander Grigoriev <agrigoriev@talend.com>
2016-05-30 09:38:46 +0200

    added started class unit test

---
+++ spec/classes/started_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'nexus::started' do
+  let(:default_parameters) {
+    {
+      'nexus_host' => 'nexus_host',
+      'nexus_port' => 9999
+    }
+  }
+
+  context 'with default parameters' do
+    let(:params) { default_parameters }
+
+    it { should contain_exec('waiting for Nexus to start').with(
+      'command' => "/usr/bin/wget --spider --tries 15 --retry-connrefused http://nexus_host:9999/nexus/"
+    ) }
+  end
+end
